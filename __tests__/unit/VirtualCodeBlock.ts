c1c84eadc298e28e42324b491adcdfeaac37f8b2
Chris Frewin <frewin.christopher@gmail.com>
2024-03-16 17:26:26 +0100

    fix unit tests

--- __tests__/unit/VirtualCodeBlock.ts
+++ __tests__/unit/VirtualCodeBlock.ts
@@ -2,12 +2,12 @@ import { describe, expect } from "@jest/globals";
 import { VirtualCodeBlock } from "../../src/VirtualCodeBlock";
 
 describe("VirtualCodeBlock", () => {
-  describe("applyActions", () => {
+  describe("applyCodeActions", () => {
     it("should add space at the beginning of a line", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([{ name: "space", value: "1" }]);
+      virtualCodeBlock.applyCodeActions([{ name: "space", value: "1" }]);
       expect(virtualCodeBlock.getCode()).toEqual(
         ' console.log("Hello World!");'
       );
@@ -17,7 +17,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "command-right", value: "1" },
         { name: "space", value: "1" },
       ]);
@@ -30,7 +30,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "arrow-right", value: "3" },
         { name: "space", value: "1" },
       ]);
@@ -43,7 +43,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "arrow-up", value: "1" },
         { name: "enter", value: "1" },
       ]);
@@ -56,7 +56,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "arrow-right", value: "8" },
         { name: "enter", value: "1" },
       ]);
@@ -70,7 +70,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([{ name: "space", value: "3" }]);
+      virtualCodeBlock.applyCodeActions([{ name: "space", value: "3" }]);
       expect(virtualCodeBlock.getCode()).toEqual(
         '   console.log("Hello World!");'
       );
@@ -80,7 +80,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([{ name: "enter", value: "3" }]);
+      virtualCodeBlock.applyCodeActions([{ name: "enter", value: "3" }]);
       expect(virtualCodeBlock.getCode()).toEqual(
         '\n\n\nconsole.log("Hello World!");'
       );
@@ -90,7 +90,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([{ name: "arrow-down", value: "1" }]);
+      virtualCodeBlock.applyCodeActions([{ name: "arrow-down", value: "1" }]);
       expect(virtualCodeBlock.getCurrentCaretPosition()).toEqual({
         row: 0,
         column: 0,
@@ -101,7 +101,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([{ name: "arrow-up", value: "1" }]);
+      virtualCodeBlock.applyCodeActions([{ name: "arrow-up", value: "1" }]);
       expect(virtualCodeBlock.getCurrentCaretPosition()).toEqual({
         row: 0,
         column: 0,
@@ -112,7 +112,7 @@ describe("VirtualCodeBlock", () => {
       const virtualCodeBlock = new VirtualCodeBlock([
         'console.log("Hello World!");',
       ]);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "arrow-right", value: "5" },
         { name: "command-left", value: "1" },
       ]);
@@ -125,7 +125,7 @@ describe("VirtualCodeBlock", () => {
     it("should bring the caret to the end of the current line with command-right", () => {
       const code = 'console.log("Hello World!");';
       const virtualCodeBlock = new VirtualCodeBlock([code]);
-      virtualCodeBlock.applyActions([{ name: "command-right", value: "1" }]);
+      virtualCodeBlock.applyCodeActions([{ name: "command-right", value: "1" }]);
       expect(virtualCodeBlock.getCurrentCaretPosition()).toEqual({
         row: 0,
         column: code.length,
@@ -134,7 +134,7 @@ describe("VirtualCodeBlock", () => {
 
     it("should handle writing multiple lines, going back up to the top line, entering a few empty spaces, then going back to the top again and writing some comments", () => {
       const virtualCodeBlock = new VirtualCodeBlock([], true);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "type-editor", value: "const someFunction = () => {" },
         { name: "enter", value: "1" },
         { name: "type-editor", value: "    console.log('hello world!')" },
@@ -161,7 +161,7 @@ const someFunction = () => {
 
     it("should show code history as expected", () => {
       const virtualCodeBlock = new VirtualCodeBlock([], true);
-      virtualCodeBlock.applyActions([
+      virtualCodeBlock.applyCodeActions([
         { name: "type-editor", value: "const someFunction = () => {" },
         { name: "enter", value: "1" },
         { name: "type-editor", value: "    console.log('hello world!')" },
